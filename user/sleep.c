908dc241fd6157e0753c501fb86c8395987138c0
LegendRegent <107825604+LegendRegent@users.noreply.github.com>
2023-12-27 11:42:02 -0600

    Add user/sleep

---
+++ user/sleep.c
@@ -0,0 +1,26 @@
+//
+// Created by Kyle Sun on 12/27/23.
+//
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    int i;
+
+    if(argc < 2){
+        fprintf(2, "Usage: sleep time...\n");
+        exit(1);
+    }
+
+    int n = atoi(argv[i]);
+
+    //what is the return value of sleep?
+    test = sleep(n);
+    // delete this afterwards
+    fprintf(1, "sleep return value: %d\n", test);
+
+    exit(0);
+}
