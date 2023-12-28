65e850688e15fd2d78903bf3d9c63c3e9e371793
LegendRegent <107825604+LegendRegent@users.noreply.github.com>
2023-12-28 09:10:45 -0600

    Add a debugger

---
+++ user/debug.h
@@ -0,0 +1,30 @@
+//
+// Created by Kyle Sun on 12/28/23.
+//
+
+#ifndef XV6_LABS_DEBUG_H
+#define XV6_LABS_DEBUG_H
+
+
+// Define debug levels
+#define DEBUG_LEVEL_NONE    0
+#define DEBUG_LEVEL_DEBUG     1
+#define DEBUG_LEVEL_INFO     2
+
+// Set the desired debug level
+#define DEBUG_LEVEL DEBUG_LEVEL_INFO
+
+// Debug macro with conditional compilation
+#if DEBUG_LEVEL >= DEBUG_LEVEL_DEBUG
+#define DEBUG_DEBUG(message) printf("INFO: %s\n", message)
+#else
+#define DEBUG_DEBUG(message)
+#endif
+
+#if DEBUG_LEVEL >= DEBUG_LEVEL_INFO
+#define DEBUG_INFO(message) printf("Medium: %s\n", message)
+#else
+#define DEBUG_INFO(message)
+#endif
+
+#endif //XV6_LABS_DEBUG_H
