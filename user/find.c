1dd916c423be3091fd6d19d67eb53aebbdf24ca7
LegendRegent <107825604+LegendRegent@users.noreply.github.com>
2023-12-29 14:57:30 -0600

    correct some medium mistakes in find

--- user/find.c
+++ user/find.c
@@ -35,12 +35,12 @@ find(char* path, char* filename)
     struct stat st;
 
     if((fd = open(path, 0)) < 0){
-        fprintf(2, "ls: cannot open %s\n", path);
+        fprintf(2, "find: cannot open %s\n", path);
         return;
     }
 
     if(fstat(fd, &st) < 0){
-        fprintf(2, "ls: cannot stat %s\n", path);
+        fprintf(2, "find: cannot stat %s\n", path);
         close(fd);
         return;
     }
@@ -50,10 +50,11 @@ find(char* path, char* filename)
             if(strcmp(fmtname(path), filename) == 0) {
                 printf("%s\n", path);
             }
+            break;
 
         case T_DIR:
             if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
-                printf("ls: path too long\n");
+                printf("find: path too long\n");
                 break;
             }
             strcpy(buf, path);
@@ -67,7 +68,7 @@ find(char* path, char* filename)
                 memmove(p, de.name, DIRSIZ);
                 p[DIRSIZ] = 0;
                 if(stat(buf, &st) < 0){
-                    printf("ls: cannot stat %s\n", buf);
+                    printf("find: cannot stat %s\n", buf);
                     continue;
                 }
                 find(buf,filename);
